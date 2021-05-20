ca713c1209222562e5772aca5458d2af80c14ac4
Ritwik <ritwik18920@gmail.com>
2021-05-20 13:28:33 +0530

    Database

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(passport.initialize());
 
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true })
+mongoose.connect("mongodb+srv://admin-ritwik:Rishi_012@cluster0.t2i1d.mongodb.net/userDB", { useNewUrlParser: true },{ useUnifiedTopology: true })
 mongoose.set("useCreateIndex", true);
 
 
